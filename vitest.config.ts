db59e1491917cf106c3613714306ca4abec0dd0d
Teno Siswono <teno.siswono@gojek.com>
2023-03-27 21:22:28 +0700

    test: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: './tests/setup.ts',
+  },
+});
